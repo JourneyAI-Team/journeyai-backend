77a69c8d82d10dbcc2cbaf814719ff7d0a459c57
Paula Yap <pagiyap003@gmail.com>
2025-05-18 14:38:04 +0800

    feat: Created new CRM Objects: Assistant and Opportunity

---
+++ app/models/assistant.py
@@ -0,0 +1,23 @@
+import datetime as dt
+import uuid
+
+from beanie import Document
+from pydantic import Field
+
+class Assistant(Document):
+    id: str = Field(default_factory=lambda: uuid.uuid4().hex)
+
+    name: str
+    internal_name: str
+    description: str
+    developer_prompt: str
+    tool_config: dict[str, str]
+    testing: bool
+    version: str
+    
+    created_at: dt.datetime = Field(
+        default_factory=lambda: dt.datetime.now(dt.timezone.utc)
+    )
+
+    # Relations
+    organization_id: str
