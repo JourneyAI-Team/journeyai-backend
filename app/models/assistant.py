42e488fd24b1c7dc086d6c326eafe01edcfb444e
Matt <philmattdev@gmail.com>
2025-05-19 00:52:33 +0800

    style: format

--- app/models/assistant.py
+++ app/models/assistant.py
@@ -4,6 +4,7 @@ import uuid
 from beanie import Document
 from pydantic import Field
 
+
 class Assistant(Document):
     id: str = Field(default_factory=lambda: uuid.uuid4().hex)
 
@@ -14,7 +15,7 @@ class Assistant(Document):
     tool_config: dict[str, str]
     testing: bool
     version: str
-    
+
     created_at: dt.datetime = Field(
         default_factory=lambda: dt.datetime.now(dt.timezone.utc)
     )
