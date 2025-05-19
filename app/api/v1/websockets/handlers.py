3c5b70febb29b9cbae24a747933a7fd9b517ac45
Paula-ETG <paula.yap@everythingtogain.com>
2025-05-20 02:31:34 +0800

    feat: included user as parameter then temporarily prints it out whenever the event, 'ingest_message', is fired.

--- app/api/v1/websockets/handlers.py
+++ app/api/v1/websockets/handlers.py
@@ -1,8 +1,11 @@
 from typing import Any
 
+from app.models.user import User
 from app.utils.websocket.handlers import register_handler
 
 
 @register_handler("ingest_message")
-async def ingest_message(connection_id: str, data: dict[str, Any]):
+async def ingest_message(connection_id: str, user: User, data: dict[str, Any]):
+    # NOTE: temporary user print to check if event is called
+    print(user)
     pass
