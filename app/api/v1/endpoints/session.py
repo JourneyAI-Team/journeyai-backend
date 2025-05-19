ab30cb84626ec68eb0e913d2840fa080775ab8a9
Matt <philmattdev@gmail.com>
2025-05-19 17:24:05 +0800

    refactor: session endpoints refactor with logging

--- app/api/v1/endpoints/session.py
+++ app/api/v1/endpoints/session.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, HTTPException, status
+from loguru import logger
 
 from app.api.deps import get_current_user
 from app.models.account import Account
@@ -34,39 +35,47 @@ async def create_session(
     HTTPException
         404 if account or assistant could not be found.
     """
-    # Check if the account exists
-    account = await Account.find_one(Account.id == session.account_id)
-    if not account:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Account could not be found."
+    with logger.contextualize(
+        user_id=current_user.id, organization_id=current_user.organization_id
+    ):
+        logger.info(f"Create session request received. {session.title=}")
+
+        # Check if the account exists
+        account = await Account.find_one(Account.id == session.account_id)
+        if not account:
+            logger.warning("Account could not be found.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Account could not be found.",
+            )
+
+        # Check if the assistant exists
+        # NOTE: Temporarily commented out this check since there's no way to save and retrieve assistants yet
+        # assistant = await Assistant.find_one(
+        #     Assistant.id == session.assistant_id
+        # )
+        # if not assistant:
+        #     raise HTTPException(
+        #         status_code=status.HTTP_404_NOT_FOUND,
+        #         detail="Assistant could not be found."
+        #     )
+
+        # Create the new session
+        new_session = Session(
+            title=session.title,
+            summary=session.summary,
+            account_id=account.id,
+            # NOTE: Change to an actual assistant's id (assistant.id)
+            assistant_id="temporary_assistant_id",
+            user_id=current_user.id,
+            organization_id=current_user.organization_id,
         )
 
-    # Check if the assistant exists
-    # NOTE: Temporarily commented out this check since there's no way to save and retrieve assistants yet
-    # assistant = await Assistant.find_one(
-    #     Assistant.id == session.assistant_id
-    # )
-    # if not assistant:
-    #     raise HTTPException(
-    #         status_code=status.HTTP_404_NOT_FOUND,
-    #         detail="Assistant could not be found."
-    #     )
+        # Save the new session to the database
+        await new_session.insert()
+        logger.success(f"Session created successfully. {new_session.title=}")
 
-    # Create the new session
-    new_session = Session(
-        title=session.title,
-        summary=session.summary,
-        account_id=account.id,
-        # NOTE: Change to an actual assistant's id (assistant.id)
-        assistant_id="temporary_assistant_id",
-        user_id=current_user.id,
-        organization_id=current_user.organization_id,
-    )
-
-    # Save the new session to the database
-    await new_session.insert()
-
-    return new_session
+        return new_session
 
 
 @router.get("/{session_id}", response_model=SessionRead)
@@ -92,17 +101,26 @@ async def get_session(
     HTTPException
         404 if session could not be found.
     """
+    with logger.contextualize(
+        user_id=current_user.id,
+        organization_id=current_user.organization_id,
+        session_id=session_id,
+    ):
+        logger.info("Retrieve session request received.")
 
-    # Check if session exists
-    session = await Session.find_one(
-        Session.id == session_id,
-        Session.organization_id == current_user.organization_id,
-    )
-    if not session:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Session could not be found."
+        # Check if session exists
+        session = await Session.find_one(
+            Session.id == session_id,
+            Session.organization_id == current_user.organization_id,
         )
-    return session
+        if not session:
+            logger.warning("Session could not be found.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Session could not be found.",
+            )
+        logger.success("Session retrieved successfully.")
+        return session
 
 
 @router.patch("/{session_id}")
@@ -131,23 +149,32 @@ async def update_session(
     HTTPException
         404 if session could not be found.
     """
+    with logger.contextualize(
+        user_id=current_user.id,
+        organization_id=current_user.organization_id,
+        session_id=session_id,
+    ):
+        logger.info(f"Update session request received. {session_in=}")
 
-    # Check if session exists.
-    session = await Session.find_one(
-        Session.id == session_id,
-        Session.organization_id == current_user.organization_id,
-    )
-    if not session:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Session could not be found."
+        # Check if session exists.
+        session = await Session.find_one(
+            Session.id == session_id,
+            Session.organization_id == current_user.organization_id,
         )
+        if not session:
+            logger.warning("Session could not be found.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Session could not be found.",
+            )
 
-    # Update the account, only description is allowed to be updated
-    await session.set(
-        {Session.summary: session_in.summary, Session.title: session_in.title}
-    )
+        # Update the account, only description is allowed to be updated
+        await session.set(
+            {Session.summary: session_in.summary, Session.title: session_in.title}
+        )
+        logger.success("Session updated successfully.")
 
-    return {}
+        return {}
 
 
 @router.delete("/{session_id}")
@@ -173,15 +200,24 @@ async def delete_session(
     HTTPException
         404 if session could not be found.
     """
+    with logger.contextualize(
+        user_id=current_user.id,
+        organization_id=current_user.organization_id,
+        session_id=session_id,
+    ):
+        logger.info("Delete session request received.")
 
-    # Check if session exists.
-    session = await Session.find_one(
-        Session.id == session_id,
-        Session.organization_id == current_user.organization_id,
-    )
-    if not session:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Session could not be found."
+        # Check if session exists.
+        session = await Session.find_one(
+            Session.id == session_id,
+            Session.organization_id == current_user.organization_id,
         )
-    await session.delete()
-    return {}
+        if not session:
+            logger.warning("Session could not be found.")
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Session could not be found.",
+            )
+        await session.delete()
+        logger.success("Session deleted successfully.")
+        return {}
