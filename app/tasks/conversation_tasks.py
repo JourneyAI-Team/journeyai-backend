dd88353d61f7891e58101cd6b0d8b528a5f28fda
matt <matt@everythingtogain.com>
2025-05-20 23:21:23 +0800

    feat: scaffolding for ingest_message celery task

--- app/tasks/conversation_tasks.py
+++ app/tasks/conversation_tasks.py
@@ -1,1 +1,13 @@
+import os
 
+from app.tasks.worker import celery_app
+
+# Testing Variables
+USE_TEST_DATA_FOR_INGESTION: bool = (
+    os.getenv("USE_TEST_DATA_FOR_INGESTION", "false").lower() == "true"
+)
+
+
+@celery_app.task(name="ingest_message")
+async def ingest_message(message_id: str):
+    pass
