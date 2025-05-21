f66a371ff2de8db31ebb75447009272367308b6f
Paula-ETG <paula.yap@everythingtogain.com>
2025-05-22 03:26:32 +0800

    feat: created function to initialize qdrant collections

---
+++ app/db/init_qdrant_db.py
@@ -0,0 +1,15 @@
+from qdrant_client import models
+
+from app.utils.qdrant_client import get_async_qdrant_client
+
+
+async def init_qdrant_db():
+    """
+    Initialize qdrant collections
+    """
+    client = get_async_qdrant_client()
+    if not await client.collection_exists("Artifacts"):
+        await client.create_collection(
+            collection_name="Artifacts",
+            vectors_config=models.VectorParams(size=4, distance=models.Distance.COSINE),
+        )
