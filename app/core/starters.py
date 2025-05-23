7af318f4447531aec09f1cb343157b1d842a803c
matt <matt@everythingtogain.com>
2025-05-23 19:31:31 +0800

    feat: create starters file that contains starters

---
+++ app/core/starters.py
@@ -0,0 +1,70 @@
+from loguru import logger
+
+from app.core.config import settings
+from app.db.init_mongo import init_db
+from app.db.init_qdrant import init_qdrant_db
+from app.utils.loki_logger import setup_logger
+from app.utils.websocket.redis_listener import start_redis_listener
+
+
+async def setup_loki_logger():
+    """
+    Set up the Loki logger.
+
+    This function sets up the Loki logger if the LOKI_URL is configured.
+    """
+
+    if settings.LOKI_URL:
+        setup_logger(
+            settings.LOKI_URL,
+            labels={"job": "journey", "environment": settings.ENVIRONMENT},
+        )
+
+
+async def initialize_app():
+    """
+    Initialize the main application.
+
+    This function sets up the Loki logger, initializes the MongoDB and Qdrant
+    databases, and starts the Redis listener for WebSocket messages.
+    """
+    await setup_loki_logger()
+
+    logger.info("Initializing app...")
+
+    logger.info("Initializing database...")
+    await init_db()
+    logger.info("Database initialized successfully")
+
+    logger.info("Initializing Qdrant database...")
+    await init_qdrant_db()
+    logger.info("Qdrant database initialized successfully")
+
+    logger.info("Starting Redis listener...")
+    await start_redis_listener()
+    logger.info("Redis listener started successfully")
+
+
+async def initialize_worker(name: str):
+    """
+    Initialize a worker group.
+
+    This function sets up the Loki logger and initializes the MongoDB and Qdrant
+    databases for a specific worker group.
+
+    Parameters
+    ----------
+    name : str
+        The name of the worker group to initialize.
+    """
+    await setup_loki_logger()
+
+    logger.info(f"Initializing worker '{name}'...")
+
+    logger.info("Initializing database...")
+    await init_db()
+    logger.info("Database initialized successfully")
+
+    logger.info("Initializing Qdrant database...")
+    await init_qdrant_db()
+    logger.info("Qdrant database initialized successfully")
