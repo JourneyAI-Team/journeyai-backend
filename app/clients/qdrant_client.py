a4267c65751526a15af925f1a3a6a26003342278
matt <matt@everythingtogain.com>
2025-05-23 19:07:04 +0800

    refactor: qdrant_client.py

--- app/clients/qdrant_client.py
+++ app/clients/qdrant_client.py
@@ -1,9 +1,11 @@
-from typing import List
-import qdrant_client
+# pylint: disable=E0611,W0406
 
-from qdrant_client.conversions.common_types import Points
+from typing import List
+
 from loguru import logger
-import qdrant_client.models
+from qdrant_client import AsyncQdrantClient
+from qdrant_client.models import PointStruct
+
 from app.core.config import settings
 
 _qdrant_async_client = None
@@ -11,7 +13,9 @@ _qdrant_async_client = None
 
 def get_async_qdrant_client():
     global _qdrant_async_client
-    _qdrant_async_client = qdrant_client.AsyncQdrantClient(url=settings.QDRANT_URL)
+    _qdrant_async_client = AsyncQdrantClient(
+        url=settings.QDRANT_URL, api_key=settings.QDRANT_API_KEY
+    )
     return _qdrant_async_client
 
 
@@ -20,9 +24,7 @@ async def insert_vector(
 ):
     client = get_async_qdrant_client()
     try:
-        points_struct = qdrant_client.models.PointStruct(
-            id=id, payload=payload, vector=vector
-        )
+        points_struct = PointStruct(id=id, payload=payload, vector=vector)
         await client.upsert(collection_name=collection_name, points=[points_struct])
     except Exception as e:
         logger.exception(f"Error inserting vector: {str(e)}")
