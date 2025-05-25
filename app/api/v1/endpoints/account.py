fec4ab1ff25069da82e26da40db67a0b0ce1e6a4
matt <matt@everythingtogain.com>
2025-05-26 02:49:12 +0800

    refactor: clean up endpoints/account.py

--- app/api/v1/endpoints/account.py
+++ app/api/v1/endpoints/account.py
@@ -1,5 +1,3 @@
-from typing import List
-
 from fastapi import APIRouter, Depends, HTTPException, status
 from loguru import logger
 
@@ -11,30 +9,24 @@ from app.schemas.account import AccountCreate, AccountRead, AccountUpdate
 router = APIRouter()
 
 
-@router.post("/")
+@router.post(
+    "/",
+    status_code=status.HTTP_201_CREATED,
+    response_model=AccountRead,
+    description="Create an account under an organization.",
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "description": "Account with the same name already exists."
+        },
+        status.HTTP_500_INTERNAL_SERVER_ERROR: {
+            "description": "Error creating account."
+        },
+    },
+)
 async def create_account(
     account: AccountCreate,
     current_user: User = Depends(get_current_user),
 ):
-    """
-    Create an account under an organization.
-
-    Parameters
-    -----
-    account: AccountCreate
-        Request body. Fields required to create an account.
-
-    Returns
-    -----
-    dict
-        Empty dictionary
-
-    Raises
-    -----
-    HTTPException
-        400 if the account is already registered in the organization
-    """
-
     with logger.contextualize(
         user_id=current_user.id,
         organization_id=current_user.organization_id,
@@ -79,22 +71,15 @@ async def create_account(
         return new_account
 
 
-@router.get("/", response_model=List[AccountRead])
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=list[AccountRead],
+    description="List accounts in an organization.",
+)
 async def list_accounts(
     current_user: User = Depends(get_current_user),
 ):
-    """
-    List accounts in an organization.
-
-    Parameters
-    -----
-
-    Returns
-    -----
-    list(AccountRead)
-        List of account object dictionaries.
-
-    """
 
     with logger.contextualize(
         user_id=current_user.id, organization_id=current_user.organization_id
@@ -110,28 +95,21 @@ async def list_accounts(
         return accounts
 
 
-@router.get("/{account_id}", response_model=AccountRead)
+@router.get(
+    "/{account_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=AccountRead,
+    description="Retrieve an account in an organization by its id.",
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Account could not be found within the organization."
+        },
+    },
+)
 async def get_account(
     account_id: str,
     current_user: User = Depends(get_current_user),
 ):
-    """
-    Retrieve an account in an organization by its id.
-
-    Parameters
-    -----
-
-    Returns
-    -----
-    AccountRead
-        The account object.
-
-    Raises
-    -----
-    HTTPException
-        404 if account could not be found within an organization
-    """
-
     with logger.contextualize(
         user_id=current_user.id,
         organization_id=current_user.organization_id,
@@ -154,32 +132,22 @@ async def get_account(
         return account
 
 
-@router.patch("/{account_id}")
+@router.patch(
+    "/{account_id}",
+    status_code=status.HTTP_200_OK,
+    response_model=AccountRead,
+    description="Update an account in an organization by its id.",
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Account could not be found within the organization."
+        },
+    },
+)
 async def update_account(
     account_id: str,
     account_in: AccountUpdate,
     current_user: User = Depends(get_current_user),
 ):
-    """
-    Update an account in an organization by its id.
-
-    Parameters
-    -----
-    account_id: str
-        Path parameter. The account id to update.
-    account_in: AccountUpdate
-        Request Body. Contains the description field.
-
-    Returns
-    -----
-    dict
-        Empty Dictionary
-
-    Raises
-    -----
-    HTTPException
-        404 if account could not be found within an organization
-    """
 
     with logger.contextualize(
         user_id=current_user.id,
@@ -200,36 +168,26 @@ async def update_account(
                 detail="Account could not be found within the organization.",
             )
 
-        # Update the account, only description is allowed to be updated
-        await account.set({Account.description: account_in.description})
+        await account.set(account_in)
         logger.success("Account updated successfully.")
 
-    return {}
+    return account
 
 
-@router.delete("/{account_id}")
+@router.delete(
+    "/{account_id}",
+    status_code=status.HTTP_204_NO_CONTENT,
+    description="Delete an account under an organization.",
+    responses={
+        status.HTTP_404_NOT_FOUND: {
+            "description": "Account could not be found within the organization."
+        },
+    },
+)
 async def delete_account(
     account_id: str,
     current_user: User = Depends(get_current_user),
 ):
-    """
-    Delete an account under an organization.
-
-    Parameters
-    -----
-    account_id: str
-        The id of the account to delete.
-
-    Returns
-    -----
-    dict
-        Empty Dictionary
-
-    Raises
-    -----
-    HTTPException
-        404 if account could not be found within an organization
-    """
 
     with logger.contextualize(
         user_id=current_user.id,
@@ -253,5 +211,3 @@ async def delete_account(
         # Delete the account
         await account.delete()
         logger.success("Account deleted successfully.")
-
-    return {}
