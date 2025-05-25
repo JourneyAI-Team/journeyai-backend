1aac95f2d47aeae1dced907f7e414658c6abc857
matt <matt@everythingtogain.com>
2025-05-26 07:25:56 +0800

    feat: send message back to websocket on process_session

--- app/workers/agents/tasks.py
+++ app/workers/agents/tasks.py
@@ -1,9 +1,15 @@
 from loguru import logger
 
 from app.models.session import Session
+from app.utils.websocket.communications import send_to_websocket
 
 
 async def process_session(ctx, connection_id: str, session_id: str):
+
+    await send_to_websocket(
+        connection_id, "processing_session", {"session_id": session_id}
+    )
+
     session = await Session.get(session_id)
 
     if not session:
