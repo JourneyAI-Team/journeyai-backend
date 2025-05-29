6f38badcdfab61a9aea90f5b9d7a6dc6ce120a03
Paula-ETG <paula.yap@everythingtogain.com>
2025-05-30 05:44:46 +0800

    feat: implement save artifact, and include it into the agent as a tool

--- app/tools/internal/save_artifact.py
+++ app/tools/internal/save_artifact.py
@@ -1,11 +1,27 @@
 from typing import Callable
+
 from agents import function_tool
 
+from app.clients.arq_client import get_arq
+from app.models.artifact import Artifact
+from app.schemas.types import OriginType
 
-# TODO: implement tools
+
+# TODO: provide a proper description
 @function_tool
-def save_artifact():
-    pass
+async def save_artifact(
+    artifact_type: str, title: str, body: str, origin_type: OriginType = "llm"
+):
+    """ """
+    arq = await get_arq()
+
+    new_artifact = Artifact(
+        type=artifact_type, title=title, body=body, origin_type=origin_type
+    )
+    await new_artifact.insert()
+    await arq.enqueue_job(
+        "post_artifact_creation", new_artifact.id, _queue_name="artifacts"
+    )
 
 
 def get_tool() -> Callable:
--- app/utils/assistant_utils.py
+++ app/utils/assistant_utils.py
@@ -2,11 +2,16 @@ from agents import Agent
 
 from app.models.assistant import Assistant
 from app.schemas.agent_context import AgentContext
+from app.tools.internal.save_artifact import save_artifact
 
 
 def get_agent_from_assistant(assistant: Assistant) -> Agent:
+
     return Agent[AgentContext](
         name=assistant.name,
         model=assistant.model,
+        tools=[
+            save_artifact,
+        ],
         instructions=assistant.developer_prompt,  # TODO Support dynamic instructions such as pulling Artifact context
     )
