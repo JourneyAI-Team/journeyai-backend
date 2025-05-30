0d85b7d13b5e2642508b499f56ed8937a3710f69
matt <matt@everythingtogain.com>
2025-05-30 17:22:37 +0800

    feat: update get_agent_from_assistants to include tool construction

--- app/utils/assistant_utils.py
+++ app/utils/assistant_utils.py
@@ -2,17 +2,23 @@ from agents import Agent, WebSearchTool
 
 from app.models.assistant import Assistant
 from app.schemas.agent_context import AgentContext
-from app.tools.internal.save_artifact import save_artifact
+from app.utils.tool_utils import get_tool
 
 
 def get_agent_from_assistant(assistant: Assistant) -> Agent:
 
+    # Retrieve tools
+    tools = []
+    for tool in assistant.tool_config["tools"]:
+        match tool["name"]:
+            case "openai_web_search":
+                tools.append(WebSearchTool())
+            case _:
+                tools.append(get_tool(tool["name"], tool["type"]))
+
     return Agent[AgentContext](
         name=assistant.name,
         model=assistant.model,
-        tools=[
-            WebSearchTool(),
-            save_artifact,
-        ],
+        tools=tools,
         instructions=assistant.developer_prompt,  # TODO Support dynamic instructions such as pulling Artifact context
     )
