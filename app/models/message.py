254a6f41c438d1c64a1b7598e7ab2b9cdbacc308
matt <matt@everythingtogain.com>
2025-06-05 20:07:17 +0800

    feat: add post-insert action to enqueue message processing

--- app/models/message.py
+++ app/models/message.py
@@ -1,9 +1,10 @@
 import datetime as dt
 import uuid
 
-from beanie import Document
+from beanie import Document, Insert, after_event
 from pydantic import Field
 
+from app.clients.arq_client import get_arq
 from app.schemas.message import InputMessageSchema
 from app.schemas.types import SenderType
 
@@ -19,9 +20,21 @@ class Message(Document):
         default_factory=lambda: dt.datetime.now(dt.timezone.utc)
     )
 
+    # Post Actions
+    embed_after_insert: bool = Field(default=True, exclude=True)
+
     # Relations
     user_id: str
     organization_id: str
     session_id: str
     assistant_id: str
     account_id: str
+
+    @after_event(Insert)
+    async def post_message_creation(self):
+        arq = await get_arq()
+        await arq.enqueue_job(
+            "post_message_creation",
+            self.id,
+            _queue_name="messages",
+        )
