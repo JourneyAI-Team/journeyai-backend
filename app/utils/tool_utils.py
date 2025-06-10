4d20efa3ed38472dcde65ebc1117bc6e0af0976b
matt <matt@everythingtogain.com>
2025-06-10 18:50:56 +0800

    fix: use tool.value

--- app/utils/tool_utils.py
+++ app/utils/tool_utils.py
@@ -7,5 +7,5 @@ def get_tool(
     tool_name: str,
     type: ToolType,
 ):
-    module = importlib.import_module(f"app.tools.{type}.{tool_name}")
+    module = importlib.import_module(f"app.tools.{type.value}.{tool_name}")
     return module.get_tool()
